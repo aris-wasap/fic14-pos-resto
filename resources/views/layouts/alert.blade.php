429ddac3c13cf10a998c8c5b442c85ed5c7d987b
aris-wasap <aristawahyusaputra@hotmail.com>
2024-03-07 11:19:17 +0700

    Ok Upload

---
+++ resources/views/layouts/alert.blade.php
@@ -0,0 +1,25 @@
+@if ($message = Session::get('success'))
+    <div class="alert alert-success alert-dismissible show fade">
+        <div class="alert-body">
+            <button class="close" data-dismiss="alert">
+                <span>×</span>
+            </button>
+            <p>{{ $message }}</p>
+        </div>
+    </div>
+@endif
+
+@if ($errors->any())
+    <div class="alert alert-danger alert-dismissible show fade">
+        <div class="alert-body">
+            <button class="close" data-dismiss="alert">
+                <span>×</span>
+            </button>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    </div>
+@endif
